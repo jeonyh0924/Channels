2009ef5eac0c08780a3834268b38e9af5cf1c9cd
Jeon Young Hoon <hungyb0924@gmail.com>
2020-04-16 21:16:16 +0900

    동기식 채팅

--- config/settings.py
+++ config/settings.py
@@ -38,9 +38,18 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django_extensions',
 
     'channels',
 ]
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
